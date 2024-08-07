f1fbedb0e86a75178aaa3700d7560254c75261af
Ali omidi <saeedd5@yahoo.com>
2024-08-07 07:00:15 +0330

    Edit Text with label

--- view/component/label_text.py
+++ view/component/label_text.py
@@ -2,29 +2,19 @@ from tkinter import *
 
 
 class TextWithLabel:
-    def __init__(self, master, text,x,y,distance=100, disabled = False, data_type="str"):
+    def __init__(self, master, text, x, y, distance=100, disabled=False):
         self.master = master
         self.text = text
         self.x = x
         self.y = y
         self.distance = distance
-        self.variable = StringVar()
-
-        match data_type:
-            case "str":
-                self._variable = StringVar()
-            case "int":
-                self._variable = IntVar()
-            case "float":
-                self._variable = DoubleVar()
-            case "bool":
-                self._variable = BooleanVar()
+        self._variable = StringVar(master)
 
         Label(master, text=text).place(x=x, y=y)
 
         if disabled:
-            self.text_box = Entry(master, textvariable=self.variable,state="readonly")
+            self.text_box = Entry(master, textvariable=self._variable, state="readonly")
             self.text_box.place(x=x + distance, y=y)
         else:
-            self.text_box = Entry(master, textvariable=self.variable)
+            self.text_box = Entry(master, textvariable=self._variable)
             self.text_box.place(x=x + distance, y=y)
